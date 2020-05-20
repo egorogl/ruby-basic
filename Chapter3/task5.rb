b3a9ae3687749875466324f895ebe057d5e0e1b6
egorogl <g0g3nZz0>
2020-05-20 22:42:31 +0500

    Refactor as kill feature

--- Chapter3/task5.rb
+++ Chapter3/task5.rb
@@ -1,7 +1,7 @@
 month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
 
 print 'Введите дату в формате дд.мм.гггг: '
-day, month, year = gets.chomp.split('.').map { |num| num.to_i }
+day, month, year = gets.chomp.split('.').map(&:to_i)
 
 month_days[1] = 29 if year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
 
