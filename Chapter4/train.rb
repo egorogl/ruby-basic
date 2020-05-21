23f42a9361269315dbff9256f154d02df5ed8016
egorogl <egorogl@gmail.com>
2020-05-21 12:22:03 +0500

    Finished write trains classes and add file for testing

--- Chapter4/train.rb
+++ Chapter4/train.rb
@@ -3,7 +3,7 @@
 # Train class
 class Train
   attr_accessor :speed
-  attr_reader :count_wagons
+  attr_reader :count_wagons, :number, :current_station, :type
 
   def initialize(number, type, count_wagons)
     @number = number
@@ -11,6 +11,7 @@ class Train
     @count_wagons = count_wagons
     @speed = 0
     @route = nil
+    @current_station = nil
   end
 
   def stop
@@ -27,12 +28,45 @@ class Train
 
   def route=(route)
     @route = route
+    @current_station = @route.stations.first
+    @current_station&.take_train(self)
+  end
 
-    route.stations.first.take_train(self)
+  def goto_next_station
+    @current_station.send_train(self)
+    @current_station = next_station
+    @current_station&.take_train(self)
+  end
+
+  def goto_prev_station
+    @current_station.send_train(self)
+    @current_station = prev_station
+    @current_station&.take_train(self)
+  end
+
+  def next_station
+    return if @route.nil?
+
+    index_next_station_in_route = @route.stations.index(@current_station) + 1
+
+    return if index_next_station_in_route > @route.stations.size - 1
+
+    @route.stations[index_next_station_in_route]
+  end
+
+  def prev_station
+    return if @route.nil?
+
+    index_prev_station_in_route = @route.stations.index(@current_station) - 1
+
+    return if index_prev_station_in_route.negative?
+
+    @route.stations[index_prev_station_in_route]
+  end
+
+  def sibling_stations
+    return if @route.nil?
+
+    [prev_station, @current_station, next_station]
   end
 end
-
-=begin
-Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
-Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
-=end
