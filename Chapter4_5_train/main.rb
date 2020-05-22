d746911766491679097c9e7546f7fb7745be06af
egorogl <egorogl@gmail.com>
2020-05-22 13:56:21 +0500

    RzdManager in progress

--- Chapter4_5_train/main.rb
+++ Chapter4_5_train/main.rb
@@ -1,12 +1,3 @@
-require_relative 'train'
-require_relative 'route'
-require_relative 'station'
-require_relative 'wagon'
-require_relative 'train'
-require_relative 'cargo_train'
-require_relative 'cargo_wagon'
-require_relative 'passenger_train'
-require_relative 'passenger_wagon'
 require_relative 'rzd_manager'
 
-RzdManager.new.start
+RzdManager.new.run
