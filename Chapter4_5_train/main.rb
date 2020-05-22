d706879885f2d45197d2b1289133c976bc85f056
egorogl <egorogl@gmail.com>
2020-05-22 11:49:54 +0500

    RzdManager finish

--- Chapter4_5_train/main.rb
+++ Chapter4_5_train/main.rb
@@ -1,3 +1,12 @@
+require_relative 'train'
+require_relative 'route'
+require_relative 'station'
+require_relative 'wagon'
+require_relative 'train'
+require_relative 'cargo_train'
+require_relative 'cargo_wagon'
+require_relative 'passenger_train'
+require_relative 'passenger_wagon'
 require_relative 'rzd_manager'
 
-RzdManager.new.run
+RzdManager.new.start
